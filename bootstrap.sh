d070a0dfd49cc78a4af40db2dc563119aaae173c
Petr Michalec <petr_michalec@cz.ibm.com>
2015-11-13 16:59:32 +0100

    improve bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,33 @@
 #!/bin/bash -ex
 
-#TODO: Install zsh
 #TODO: update to chef recipe
+
+test -e /usr/bin/apt-get && {
+    sudo apt-get install -y zsh fish
+} || {
+    sudo yum install -y zsh fish
+}
+
 
 test -e $HOME/.homesick/repos/dotshell || {
     homeshick clone --batch git://github.com/epcim/dotshell.git
 }
 
-#zsh & zsh powerline theme
+# fish & oh-my-fish
+curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
+
+# zsh & zsh powerline theme
 homeshick clone --batch robbyrussell/oh-my-zsh
 homeshick clone --batch https://github.com/jeremyFreeAgent/oh-my-zsh-powerline-theme.git
+homeshick clone --batch https://github.com/seebi/dircolors-solarized
 
 homeshick link dotshell
 
 ln -s $HOME/.homesick/repos/oh-my-zsh-powerline-theme/powerline.zsh-theme $HOME/.oh-my-zsh/themes/
+ln -s $HOME/.homesick/repos/dircolors-solarized/dircolors.256dark $HOME/.dircolors
+ln -s $HOME/.homesick/repos/dircolors-solarized/dircolors.256dark $HOME/.dir_colors
 
-which zsh && chsh -s $(which zsh)
+# set default shell
+#which zsh && chsh -s $(which zsh)
+which fish && chsh -s $(which fish)
 
