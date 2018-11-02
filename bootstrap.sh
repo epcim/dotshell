64e39c306b61f48fb29234ff7b85e0b768357fe6
Petr Michalec <epcim@apealive.net>
2018-11-02 20:43:35 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -ex
 
-#TODO: update to chef recipe
-
-test -e /usr/bin/apt-get && {
-    sudo apt-get install -y zsh fish
-} || {
-    sudo yum install -y zsh fish
-}
-
+if [[ -e /usr/bin/apt-get ]]; then
+    sudo apt-get install -y git zsh fish curl
+elif [[ -e /usr/bin/yum ]]; then
+    sudo yum install -y git zsh fish curl
+elif [[ -e /usr/local/bin/brew ]]; then
+    brew install git zsh fish homeshick
+fi
 
 test -e $HOME/.homesick/repos/dotshell || {
     homeshick clone --batch git://github.com/epcim/dotshell.git
@@ -27,14 +26,15 @@ ln -s $HOME/.homesick/repos/oh-my-zsh-powerline-theme/powerline.zsh-theme $HOME/
 ln -s $HOME/.homesick/repos/dircolors-solarized/dircolors.256dark $HOME/.dircolors
 ln -s $HOME/.homesick/repos/dircolors-solarized/dircolors.256dark $HOME/.dir_colors
 
+
+#TODO, remaining move to salt workspace/worksrtation model
 # set default shell
 #which zsh && chsh -s $(which zsh)
-which fish && chsh -s $(which fish)
-
+#which fish && chsh -s $(which fish)
 
 # install z
-wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/bin/z.sh
-chmod u+x ~/bin/z.sh
+#curl https://raw.githubusercontent.com/rupa/z/master/z.sh -o ~/bin/z.sh
+#chmod u+x ~/bin/z.sh
 
 # install fisherman for fish
-npm i -g fisherman # https://github.com/fisherman/fisherman
+#npm i -g fisherman # https://github.com/fisherman/fisherman
